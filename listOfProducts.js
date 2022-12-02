8b9587ab919908b3823e9bac10076dce7c13f676
“mannannavar” <sagar.cs21@bmsce.ac.in>
2022-12-02 23:47:50 +0530

    added navbar

---
+++ listOfProducts.js
@@ -0,0 +1,31 @@
+// console.log("hi");
+// console.log("hello");
+// // let name = prompt("What is your name?", "Guest");
+// // console.log(name);
+// const ls = ['wifi', '5g', 'dual-camera']
+// const brand = ['nokia', 'samsung', 'lenovo']
+// const mobiles = []
+// for(let i=0;i<10;i++){
+//     let mobile = {
+//         "brand": brand[Math.floor(Math.random()*brand.length)],
+//         "model": i*10+1,
+//         "features":[]
+//     }
+//     const features = []
+//     for(let i=0;i<ls.length;i++){
+//         const x = Math.random()
+//         if(x >= 0.3){
+//             features.push(ls[i])
+//         }
+//     }
+//     mobile.features = features
+//     mobiles.push(mobile)
+// }
+// const data = mobiles
+
+// let list = document.getElementById("ul-list");
+// data.forEach((item)=>{
+//     let li = document.createElement("li");
+//     li.innerText = item.brand;
+//     list.appendChild(li);
+//   })
