2a7aa03b1ce9438e09ec9c55f4084266b530dca2
sagarcm13 <sagar.cs21@bmsce.ac.in>
2022-12-10 21:22:34 +0530

    list of product page 95% done

--- listOfProducts.js
+++ listOfProducts.js
@@ -1,31 +1,10 @@
-// console.log("hi");
-// console.log("hello");
-// // let name = prompt("What is your name?", "Guest");
-// // console.log(name);
-// const ls = ['wifi', '5g', 'dual-camera']
-// const brand = ['nokia', 'samsung', 'lenovo']
-// const mobiles = []
-// for(let i=0;i<10;i++){
-//     let mobile = {
-//         "brand": brand[Math.floor(Math.random()*brand.length)],
-//         "model": i*10+1,
-//         "features":[]
-//     }
-//     const features = []
-//     for(let i=0;i<ls.length;i++){
-//         const x = Math.random()
-//         if(x >= 0.3){
-//             features.push(ls[i])
-//         }
-//     }
-//     mobile.features = features
-//     mobiles.push(mobile)
-// }
-// const data = mobiles
-
-// let list = document.getElementById("ul-list");
-// data.forEach((item)=>{
-//     let li = document.createElement("li");
-//     li.innerText = item.brand;
-//     list.appendChild(li);
-//   })
+function onChanging(value) {
+    if (window.location.href.indexOf("x=1") > -1) {
+        js1Function(value);
+    } else if (window.location.href.indexOf("x=2") > -1) {
+        js2Function(value);
+    } else if (window.location.href.indexOf("x=3") > -1) {
+        js3Function(value);
+    } else
+        js4Function(value);
+}
