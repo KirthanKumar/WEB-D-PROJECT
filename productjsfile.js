4b0755bed68ee9284e3edad8815fd6635e1ee405
sagarcm13 <sagar.cs21@bmsce.ac.in>
2023-01-05 12:54:09 +0530

    price tag

--- productjsfile.js
+++ productjsfile.js
@@ -13,14 +13,14 @@
 // </script>
 // url of page should have 2 parameters
 // array of objects containing 4 img url,description,key features,pricetag
-let arr=
-    {
+let arr={
         "imgurl1": "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666096848/Croma%20Assets/Computers%20Peripherals/Laptop/Images/263843_lyw43f.png/mxw_2256,f_auto",
         "imgurl2":"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666096848/Croma%20Assets/Computers%20Peripherals/Laptop/Images/263843_lyw43f.png/mxw_2256,f_auto",
         "imgurl3":"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1668663643/Croma%20Assets/Computers%20Peripherals/Laptop/Images/262842_2_s8fnov.png/mxw_2256,f_auto",
         "imgurl4":"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1668663647/Croma%20Assets/Computers%20Peripherals/Laptop/Images/262842_3_sadei3.png/mxw_2256,f_auto",
         "imgurl5":"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1668663640/Croma%20Assets/Computers%20Peripherals/Laptop/Images/262842_1_rpu5fd.png/mxw_2256,f_auto",
-        heading:"Acer Aspire 3 Intel Core i3 11th Gen (15.6 inch, 8GB, 512GB, Windows 11 Home, MS Office 2021, Intel UHD, Full HD LED-Backlit Display, Silver, NX.ADDSI.011)\n\n₹ 20,000",
+        "heading":"Acer Aspire 3 Intel Core i3 11th Gen (15.6 inch, 8GB, 512GB, Windows 11 Home, MS Office 2021, Intel UHD, Full HD LED-Backlit Display, Silver, NX.ADDSI.011) ",
+        price:"₹ 20,000",
         features1: "Display: 39.62 cms (15.6 inches), FHD LED-Backlit",
         features2:"Memory: 8GB DDR4, 512GB SSD ROM",
         features3:"Processor: Intel Core i3 11th Generation",
@@ -88,16 +88,19 @@ function productfilling(){
     let h2=document.createElement("h2");
     h2.innerHTML=arr.heading;
     div1.append(h2);
-    // let div11=document.createElement("div");
-    // div11.setAttribute("id", "price");
-    // let h3=document.createElement("h3");
-    // h3.innerHTML=arr.price;
+    let div11=document.createElement("div");
+    div11.setAttribute("id", "price");
+    let h3=document.createElement("h3");
+    h3.innerHTML=arr.price;
+    div11.append(h3);
+    div1.append(div11);
     let div13=document.createElement("div");
     div13.setAttribute("class", "borderbox");
-    // let p=document.createElement("p2");
-    // p.setAttribute("class", "pr");
-    // document.getElementsById("pr").innerHTML("Key Features");
-    // div13.append(p);
+
+    let p2=document.createElement("p2");
+    p2.innerHTML="Key features";
+    div13.append(p2);
+
     let u=document.createElement("ul");
     u.setAttribute("class", "p");
     let li1=document.createElement("li");
